a8fb1a8540eef7f621bc2d24576f5158600f2647
Matej Kovac <matej.kovac@gmail.com>
2016-07-23 12:30:51 +0200

    check.environ... fns

--- functions.sh
+++ functions.sh
@@ -9,6 +9,38 @@ log() {
   return 0
 }
 
+check.environ() {
+  test $# -ge 1 || {
+    log "check.environ() expects environment variable names as arguments"
+    exit 1
+  }
+
+  while test $# -ge 1; do
+    local name=$1
+    shift
+    local check=${!name:-}
+    test "${check}" && continue;
+    log "Error: required variable '$name' was not set"
+    exit 1
+  done
+}
+
+check.environ.isfile() {
+  test $# -ge 1 || {
+    log "check.environ.isfile() expects environment variable names as arguments"
+    exit 1
+  }
+
+  while test $# -ge 1; do
+    local name=$1
+    shift
+    check.environ ${name}
+    test "${!name}" && continue;
+    log "Error: '$name' should point to a file"
+    exit 1
+  done
+}
+
 include() {
   test $# -eq 1 || {
     log 'missing argument to include()'
