c3a20bd85aa157e6e05411d5182162822bc53284
Matej Kovac <matej.kovac@t-systems.sk>
2016-09-07 13:50:35 +0200

    .

--- functions.sh
+++ functions.sh
@@ -93,7 +93,7 @@ tplrender() {
   }
 
   while true; do
-    read -r line || {true;break;}
+    read -r line || {true;break}
     while [[ "$line" =~ (\$\{[a-zA-Z_][a-zA-Z_0-9]*\}) ]] ; do
       LHS=${BASH_REMATCH[1]}
       RHS=${!LHS:-}
